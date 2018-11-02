41aec80b0f46b319b901a5c3a643a82cb25429e1
JBallin <JBallin@users.noreply.github.com>
2018-11-01 18:35:27 -0700

    Fix spacing

--- app/shopping-cart/routes.js
+++ app/shopping-cart/routes.js
@@ -28,10 +28,10 @@ router.get('/items', (req, res, next) => {
 })
 
 router.post('/items', (req, res, next) => {
-  const item = db.items.insert({ 
-    quantity: Number(req.body.quantity), 
-    product_id: Number(req.body.product_id) 
-  })  
+  const item = db.items.insert({
+    quantity: Number(req.body.quantity),
+    product_id: Number(req.body.product_id)
+  })
   res.json(item)
 })
 
